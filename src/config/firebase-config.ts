6de483a4ce756b8808d4fca7f156c90e369c1d45
Liam Howatt <liamhowatt34@gmail.com>
2024-09-20 13:28:58 -0400

    Added dotenv functionality for hiding data

--- src/config/firebase-config.ts
+++ src/config/firebase-config.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBl7oF1ZRg7BhNk5Co36va2LB3o9UB83OE",
-  authDomain: "expense-tracker-5e943.firebaseapp.com",
-  projectId: "expense-tracker-5e943",
-  storageBucket: "expense-tracker-5e943.appspot.com",
-  messagingSenderId: "230393342015",
-  appId: "1:230393342015:web:288d04876b757713a4708a",
-  measurementId: "G-PGLFL45C83",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
