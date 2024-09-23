12419b0b9bee7bf7f4a1af2e8189b31d1f0b133b
Liam Howatt <liamhowatt34@gmail.com>
2024-09-23 17:52:23 -0400

    Finished styling main components

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -24,11 +24,13 @@ function Auth() {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-cyan-600 to-indigo-600">
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-cyan-600 to-indigo-500">
       <div className="flex flex-col w-1/3 h-80 justify-around items-center font-bold border border-azure rounded mb-60 shadow-xl bg-slate-700">
-        <p className="text-4xl font-bold text-white">Expense Tracker Sign In</p>
+        <p className="text-4xl font-bold text-slate-50">
+          Expense Tracker - Sign In
+        </p>
         <button
-          className="flex justify-center items-center h-20 w-64 p-4 mb-10 text-lg border border-azure rounded-full bg-slate-50 hover:bg-opacity-40 transition ease-in-out"
+          className="flex justify-center items-center h-20 w-64 p-4 mb-10 text-lg text-slate-50 border border-slate-50 rounded-full  hover:bg-slate-50 hover:text-slate-800 transition ease-in-out"
           onClick={signInWithGoogle}
         >
           Sign In With Google
--- src/pages/ExpenseTracker.tsx
+++ src/pages/ExpenseTracker.tsx
@@ -61,9 +61,9 @@ function ExpenseTracker() {
   const totalExpense = useMemo(() => calculateTotal("expense"), [transactions]);
 
   return (
-    <div className="flex p-12 h-screen">
-      <section className="flex flex-col justify-between items-start w-1/2 h-full p-8 bg-slate-800 text-slate-50 rounded-sm m-4">
-        <div className="flex w-full justify-between items-center font-bold">
+    <div className="flex p-12 h-screen bg-gradient-to-r from-cyan-600 to-indigo-500">
+      <section className="flex flex-col justify-center items-start w-1/2 h-full p-10 bg-slate-800 text-slate-50 rounded-sm mr-4 shadow-xl shadow-black rounded-r-lg">
+        <div className="flex w-full justify-between items-center font-bold mb-28">
           {profilePhoto && (
             <div>
               <img
@@ -73,7 +73,7 @@ function ExpenseTracker() {
               />
             </div>
           )}
-          <h1 className="text-3xl mb-2">{name}'s Expense Tracker</h1>
+          <h1 className="text-4xl mb-2 underline">{name}'s Expense Tracker</h1>
           <button
             onClick={signUserOut}
             className="flex justify-center items-center border border-slate-50 h-12 w-auto rounded-full p-4 hover:bg-slate-50 hover:text-slate-600"
@@ -81,65 +81,84 @@ function ExpenseTracker() {
             Sign Out
           </button>
         </div>
-        <div>
-          <h3>Your Balance</h3>
+        <div className="mb-12">
+          <h3 className="font-bold text-3xl underline mb-1">Balance</h3>
           {totalBalance >= 0 ? (
-            <h2>${totalBalance}</h2>
+            <h2 className="font-bold text-3xl">${totalBalance}</h2>
           ) : (
-            <h2>-${totalBalance * -1}</h2>
+            <h2 className="font-bold text-3xl">-${totalBalance * -1}</h2>
           )}
         </div>
-        <div>
+        <div className="font-bold text-xl flex w-1/4 justify-around items-center mb-20">
           <div>
-            <h4>Income</h4>
+            <h4 className="mr-12 underline">Income</h4>
             <p>${totalIncome}</p>
           </div>
           <div>
-            <h4>Expense</h4>
+            <h4 className="underline">Expense</h4>
             <p>${totalExpense}</p>
           </div>
         </div>
-        <form action="" className="flex flex-col" onSubmit={onSubmit}>
+        <form
+          action=""
+          className="flex flex-col justify-between items-start h-1/2 w-1/2 mb-2"
+          onSubmit={onSubmit}
+        >
           <input
             type="text"
             placeholder="Description"
             required
             onChange={(e) => setDescription(e.target.value)}
+            className="h-10 rounded-md p-2 text-slate-800 w-full"
           />
           <input
             type="number"
             placeholder="Amount"
             required
             onChange={(e) => setTransactionAmount(Number(e.target.value))}
+            className="h-10 rounded-md p-2 text-slate-800 w-full"
           />
-          <input
-            type="radio"
-            id="expense"
-            value="expense"
-            checked={transactionType === "expense"}
-            onChange={(e) => setTransactionType(e.target.value)}
-          />
-          <label htmlFor="expense">Expense</label>
-          <input
-            type="radio"
-            id="income"
-            value="income"
-            checked={transactionType === "income"}
-            onChange={(e) => setTransactionType(e.target.value)}
-          />
-          <label htmlFor="income">Income</label>
-          <button type="submit">Add Transaction</button>
+          <div className="flex w-20 justify-around items-center">
+            <input
+              type="radio"
+              id="expense"
+              value="expense"
+              checked={transactionType === "expense"}
+              onChange={(e) => setTransactionType(e.target.value)}
+              className="m-2"
+            />
+            <label htmlFor="expense">Expense</label>
+          </div>
+          <div className="flex w-20 justify-around items-center">
+            <input
+              type="radio"
+              id="income"
+              value="income"
+              checked={transactionType === "income"}
+              onChange={(e) => setTransactionType(e.target.value)}
+            />
+            <label htmlFor="income">Income</label>
+          </div>
+          <button
+            type="submit"
+            className="flex justify-center items-center font-bold  border border-slate-50 h-12 w-auto rounded-full p-4 hover:bg-slate-50 hover:text-slate-600"
+          >
+            Add Transaction
+          </button>
         </form>
       </section>
-      <section className="h-full w-1/2 m-4 bg-slate-800 text-slate-50 flex flex-col items-center font-bold p-4">
-        <h3 className="text-4xl mb-8">Transactions</h3>
-        <ul>
+      <section className="h-full w-1/2 ml-4 bg-slate-800 text-slate-50 flex flex-col items-start font-bold p-12 shadow-xl shadow-black rounded-l-lg">
+        <h3 className="text-4xl mb-8 mt-4 underline">Transactions</h3>
+        <ul className="overflow-auto border border-slate-50 h-3/4 w-1/2 p-4 rounded-sm">
           {transactions.map((transaction: Transaction) => {
             const { description, transactionAmount, transactionType } =
               transaction;
             return (
-              <li key={transaction.id}>
-                <h4>{description}</h4>
+              <li
+                key={transaction.id}
+                className="mb-4 border border-slate-50 p-2 rounded-lg"
+              >
+                <h4 className="underline">{description}</h4>
                 <p>
                   ${transactionAmount} {"- "}
                   <label htmlFor="">{transactionType}</label>
