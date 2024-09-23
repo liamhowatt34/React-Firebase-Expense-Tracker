e7108954bdfcb0983007db7afea077bd32bcd951
Liam Howatt <liamhowatt34@gmail.com>
2024-09-22 22:30:18 -0400

    Added styles to the Auth component

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -24,12 +24,19 @@ function Auth() {
   }
 
   return (
-    <div className="flex w-full h-full">
-      <p>Sign in With Google to Continue</p>
-      <button className="" onClick={signInWithGoogle}>
-        {" "}
-        Sign In With Google
-      </button>
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-cyan-600 to-indigo-600">
+      <div className="flex flex-col w-1/3 h-96 justify-around items-center font-bold font-mono border border-azure rounded mb-60 bg-white bg-opacity-10 backdrop-blur-xl shadow-xl">
+        <p className="text-4xl font-bold text-sky-400">
+          Expense Tracker Sign In
+        </p>
+        <button
+          className="flex justify-center items-center h-20 w-64 p-4 text-lg border border-azure rounded-full bg-slate-50 backdrop-opacity-80 hover:bg-opacity-50 hover:shadow-sm hover:shadow-white transition ease-in-out"
+          onClick={signInWithGoogle}
+        >
+          Sign In With Google
+          <span className="material-symbols-outlined ml-2">login</span>
+        </button>
+      </div>
     </div>
   );
 }
